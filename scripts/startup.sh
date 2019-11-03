4d6bef20d780503ce6fa1843938217e9a3cc06e6
hillel <hillelt@allenai.org>
2019-11-03 17:11:08 +0200

    ISSUE-4: startup.sh wasn't using the version number variable correctly

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,9 +6,10 @@ fi
 
 dataset_name=$1
 dataset_version=$2
+
 if [ ! -d /local/data/index ]
 then
-    curl https://storage.googleapis.com/ai2i/SPIKE/datasets/${dataset_name}/dataset_version/${dataset_name}.tar.gz | tar -C /local/data -xzv
+    curl https://storage.googleapis.com/ai2i/SPIKE/datasets/${dataset_name}/dataset_version/${dataset_name}-odinson-index.tar.gz | tar -C /local/data -xzv
 fi
 
 /local/bin/odinson-rest-api
