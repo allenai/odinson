db30fa46faa68821b6ce8f84d1cd85e008518ac3
hillel <hillelt@allenai.org>
2019-11-05 10:40:18 +0200

    downloading the index via direct URL

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,17 +1,15 @@
 if [ $# -ne 2 ]
   then
-    echo "USAGE: startup.sh <dataset_name> <dataset_version>"
+    echo "USAGE: startup.sh <index_url>"
     exit 1
 fi
 
-DATASET_NAME=$1
-DATASET_VERSION=$2
-DATASET_URL=https://storage.googleapis.com/ai2i/SPIKE/datasets/${DATASET_NAME}/${DATASET_VERSION}/${DATASET_NAME}-odinson-index.tar.gz
+INDEX_URL="${INDEX_URL:-https://storage.googleapis.com/ai2i/SPIKE/datasets/tacred-train-labeled/tacred-train-odinson-index-ordered-24092019.tar.gz}"
 
 if [ ! -d /local/data/index ]
 then
-    echo "Downloading dataset from: ${DATASET_URL}..."  
-    curl  ${DATASET_URL} | tar -C /local/data -xzv || exit 1
+    echo "Downloading dataset from: ${INDEX_URL}..."  
+    curl  ${INDEX_URL} | tar -C /local/data -xzv || exit 1   
 fi
 
 /local/bin/odinson-rest-api
