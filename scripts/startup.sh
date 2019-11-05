9d8c4a0647a2bf072075b1cbd3cd9afba19830dd
hillel <hillelt@allenai.org>
2019-11-05 12:39:45 +0200

    using env variables instead of CLI arguments in startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,15 +1,12 @@
-if [ $# -ne 2 ]
-  then
-    echo "USAGE: startup.sh <index_url>"
-    exit 1
-fi
-
 INDEX_URL="${INDEX_URL:-https://storage.googleapis.com/ai2i/SPIKE/datasets/tacred-train-labeled/tacred-train-odinson-index-ordered-24092019.tar.gz}"
+LOCAL_INDEX_PATH=/local/data/index
 
-if [ ! -d /local/data/index ]
+if [ ! -d ${LOCAL_INDEX_PATH} ]
 then
-    echo "Downloading dataset from: ${INDEX_URL}..."  
-    curl  ${INDEX_URL} | tar -C /local/data -xzv || exit 1   
+    echo "INFO: Downloading index from: ${INDEX_URL}..."  
+    curl  ${INDEX_URL} | tar -C /local/data -xzv || exit 1
+else
+    echo "INFO: loading existing index from ${LOCAL_INDEX_PATH}..."
 fi
 
 /local/bin/odinson-rest-api
